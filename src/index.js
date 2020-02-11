191d35a2e2529b8ebad1b2770e13019d3aee5073
Stefanie <stefcodes@gmail.com>
2020-02-11 15:05:56 -0800

    add fetch and render for title and image

---
+++ src/index.js
@@ -0,0 +1,57 @@
+const BASE_URL = 'http://localhost:3000'
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('HI-C', 'color: firebrick');
+     fetchBooks()
+    //  renderBooks(books)
+    // fetchDogBreeds()
+    // handleClickBreed()
+  })
+
+   function fetchBooks() {
+    fetch(BASE_URL+'/books')
+    .then(resp => resp.json())
+    .then(books => renderBooks(books));
+   }
+
+  function renderBooks(books) {
+    const books_container = document.querySelector('.container')
+    books_container.innerHTML = ""; 
+    books.forEach(book => {
+      let book_div = document.createElement('div')
+      let book_img = document.createElement('img')
+      let blink = `./src/${book.imageLink}`
+      book_img.src = blink
+      book_div.innerHTML += `<h4>${book.title}</h4>`
+      books_container.appendChild(book_div)
+      book_div.appendChild(book_img)
+
+    })
+  }
+
+//   function fetchDogBreeds() {
+//     fetch('https://dog.ceo/api/breeds/list/all')
+//     .then(resp => resp.json())
+//     .then(breeds => renderBreeds(Object.keys(breeds.message)));
+//   }
+
+//   function renderBreeds(breeds) {
+//     const dog_breeds = document.getElementById('dog-breeds')
+//     breeds.forEach(breed => {
+//       const li = document.createElement('li')
+//     //  li.innerHTML = `${breed}`
+//       li.innerHTML = `<p class='breed_name'>${breed}</p>`
+//     //    li.setAttribute("class", "d")
+//       dog_breeds.appendChild(li)
+//       li.style.cursor = 'pointer';
+//       li.addEventListener("click", handleClickBreed)
+//     })
+//   }
+
+//   function handleClickBreed(event) {
+//         event.target.style.color = "lightblue"; 
+//     } 
+
+    //Still Need to Filter Breeds
+  //function 
+
+// document.getElementById("myDiv").style.color = "lightblue";
